43d9d61c73795812eef5d72f33ff8a17a7e4d475
calvin <cal.villa@icloud.com>
2022-03-13 11:28:11 -0400

    idb working properly

--- public/js/index.js
+++ public/js/index.js
@@ -137,6 +137,7 @@ function sendTransaction(isAdding) {
   .catch(err => {
     // fetch failed, so save in indexed db
     saveRecord(transaction);
+    saveRecord(formData);
 
     // clear form
     nameEl.value = "";
